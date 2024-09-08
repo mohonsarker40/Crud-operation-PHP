d64adabe48e71555543972315a5f14bbaddadf34
mohon sarker <mohonsarker40@gmail.com>
2024-09-08 22:33:08 +0600

    user table added with bootstap

---
+++ user.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crud operation PHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<div class="container mt-5">
+    <form class="p-5" action="/action_page.php" method="post">
+   <div class="mb-3 mt-3">
+    <label for="name" class="form-label">Name:</label>
+    <input type="text" class="form-control" id="name" placeholder="Enter name" name="name">
+  </div>
+  <div class="mb-3 mt-3">
+    <label for="email" class="form-label">Email:</label>
+    <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+  </div>
+  <div class="mb-3 mt-3">
+    <label for="mobile" class="form-label">Mobile:</label>
+    <input type="mobile" class="form-control" id="mobile" placeholder="Enter Mobile" name="mobile">
+  </div>
+  <div class="mb-3">
+    <label for="password" class="form-label">Password:</label>
+    <input type="password" class="form-control" id="password" placeholder="Enter password" name="password">
+  </div>
+  <div class="form-check mb-3">
+    <label class="form-check-label">
+      <input class="form-check-input" type="checkbox" name="remember"> Remember me
+    </label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+
+</form>
+
+</div>
+</body>
+</html>
